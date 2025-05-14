cfd12f5bd3d0d59837d05c305fc3e2aeeba0a3ce
Akoliya Jash Vijaybhai <100468694+JashAkoliya@users.noreply.github.com>
2025-05-14 11:51:40 +0530

    Add files via upload

---
+++ bigquery_create_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE irctc_stream_tb (
+  row_key VARCHAR(MAX),
+  name VARCHAR(MAX),
+  age VARCHAR(MAX),
+  email VARCHAR(MAX),
+  join_date VARCHAR(MAX),
+  last_login VARCHAR(MAX),
+  loyalty_points VARCHAR(MAX),
+  account_balance VARCHAR(MAX),
+  is_active VARCHAR(MAX),
+  inserted_at VARCHAR(MAX),
+  updated_at VARCHAR(MAX),
+  loyalty_status VARCHAR(MAX),
+  account_age_days VARCHAR(MAX)
+);
+
